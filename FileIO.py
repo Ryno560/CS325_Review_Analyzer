19276fcb9aa87bd4e313272d65885ca0dae8a734
Ryno560 <rhanks@siue.edu>
2024-09-24 11:59:12 -0500

    Added header comments to FileIO.py

--- FileIO.py
+++ FileIO.py
@@ -1,9 +1,17 @@
+#Author: Ryan Hanks
+#File: FileIO.py
+#Description: This file does nothing when executed. 
+#The File class is created with constructor that accepts both an input and output file as arguments
+#Also declared are the member functions read and write 
+
 class File:
 
+    #File class constructor accepts an input file string and an output file string
     def __init__(self, input_file: str, output_file: str):
         self.input_file = input_file
         self.output_file = output_file
 
+    #read member function reads lines from the input file one by one and stores them into a list
     def read(self) -> str:
     #Reads from the input file.
         try:
@@ -15,7 +23,8 @@ class File:
             print(f"Error: {self.input_file} not found.")
             return ""
         
+    #write member function appends the given response string to the output file
     def write(self, response: str):
-    #Writes the model response to the output file.
+    #Writes the response string to the output file.
         with open(self.output_file, "a") as file:
             file.write(response + "\n")
