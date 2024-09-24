fcc710354a385246ddee939df8cf7714085e7fe0
Ryno560 <rhanks@siue.edu>
2024-09-24 12:01:10 -0500

    changed AI.py to contain the AI class

--- AI.py
+++ AI.py
@@ -1,24 +1,30 @@
+#Author: Ryan Hanks
+#File: AI.py
+#Description: This file does nothing when executed. 
+#The AI class is created with constructor that has no arguments that should be passed by the user
+#Has a declared member function getResponse that returns a string
+
 from llama_cpp import Llama
-from FileIO import File
 
-llm = Llama(
-  model_path="./Phi-3-mini-4k-instruct-q4.gguf",  # path to GGUF file
-  n_ctx=4096,  # The max sequence length to use - note that longer sequence lengths require much more resources
-  n_threads=8, # The number of CPU threads to use, tailor to your system and the resulting performance
-  n_gpu_layers=35, # The number of layers to offload to GPU, if you have GPU acceleration available. Set to 0 if no GPU acceleration is available on your system.
-)
+class AI:
+    #constructot declares llm which is the variable used to access the phi3 model
+    def __init__(self):
 
-file = File("input.txt", "output.txt")
-prompts = file.read()
-
-
-for prompt in prompts:
-
-    output = llm(
-    f"<|user|>\n{prompt}<|end|>\n<|assistant|>",
-    max_tokens=256,  # Generate up to 256 tokens
-    stop=["<|end|>"], 
-    echo=True,  # Whether to echo the prompt
-    )
-
-    file.write(output['choices'][0]['text'])
+        self.llm = Llama(
+        model_path="./Phi-3-mini-4k-instruct-q4.gguf",  # path to GGUF file
+        n_ctx=4096,  # The max sequence length to use - note that longer sequence lengths require much more resources
+        n_threads=8, # The number of CPU threads to use, tailor to your system and the resulting performance
+        n_gpu_layers=35, # The number of layers to offload to GPU, if you have GPU acceleration available. Set to 0 if no GPU acceleration is available on your system.
+        )
+    
+    #returns the response of the phi3 model to an input string. The returned statement is also a string type 
+    def getResponse(self, input: str) -> str:
+         
+        output = self.llm(
+        f"<|user|>\n{input}<|end|>\n<|assistant|>",
+        max_tokens=256,  # Generate up to 256 tokens
+        stop=["<|end|>"], 
+        echo=True,  # Whether to echo the prompt
+        )
+         
+        return output['choices'][0]['text']
